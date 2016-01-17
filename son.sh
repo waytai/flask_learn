9a3987219d35d1e307516178613fbb9afc02c550
wayne <278972894@qq.com>
2016-01-17 14:50:57 +0800

    flask hello

---
+++ son.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# File Name: son.sh
+# Author: wayne
+# mail: @163.com
+# Created Time: 2015年10月16日 星期五 09时51分44秒
+#########################################################################
+#!/bin/bash
+source /usr/local/bin/virtualenvwrapper.sh
+workon son
