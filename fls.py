35d4b8847ad9319a445c3c05b7f3e27703d10d02
wayne <278972894@qq.com>
2016-01-17 15:12:12 +0800

    add fls

--- fls.py
+++ fls.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    return 'Index page'
+
+@app.route('/hello')
+def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
